a8a8214e31f54912941380d76e5946755f968af2
Themaister <maister@archlinux.us>
2012-05-31 20:10:38 +0200

    Resolve ipv4/ipv6 properly.

--- gtk/connection.cpp
+++ gtk/connection.cpp
@@ -1,6 +1,7 @@
 #include "connection.hpp"
 
 #include <memory>
+#include <iostream>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -13,7 +14,7 @@
 #include <sys/poll.h>
 
 
-Connection::Connection(const std::string &host)
+Connection::Connection(const std::string &host) : fd(-1)
 {
    struct sigaction sig{};
    sig.sa_handler = SIG_IGN;
@@ -24,22 +25,33 @@ Connection::Connection(const std::string &host)
    hints.ai_socktype = SOCK_STREAM;
 
    if (getaddrinfo(host.c_str(), stringify(default_port).c_str(), &hints, &servinfo) < 0)
-      throw std::runtime_error("getaddrinfo() failed.\n");
+      throw std::runtime_error("getaddrinfo() failed.");
 
    std::unique_ptr<struct addrinfo, std::function<void (struct addrinfo*)>> holder(servinfo,
          [](struct addrinfo *info) {
             freeaddrinfo(info);
          });
 
-   fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-   if (fd < 0)
-      throw std::runtime_error("Failed to create socket.\n");
-
-   if (connect(fd, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
+   auto tmp = servinfo;
+   bool connected = false;
+   while (tmp)
    {
+      fd = socket(tmp->ai_family, tmp->ai_socktype, tmp->ai_protocol);
+      if (fd < 0)
+         throw std::runtime_error("Failed to create socket.");
+
+      if (connect(fd, tmp->ai_addr, tmp->ai_addrlen) == 0)
+      {
+         connected = true;
+         break;
+      }
+
+      tmp = tmp->ai_next;
       close(fd);
-      throw std::runtime_error("Failed to connect.\n");
    }
+
+   if (!connected)
+      throw std::runtime_error("Failed to connect.");
 }
 
 Connection::~Connection()
@@ -50,23 +62,23 @@ Connection::~Connection()
 std::string Connection::command(const std::string &cmd)
 {
    if (write(fd, cmd.data(), cmd.size()) < static_cast<ssize_t>(cmd.size()))
-      throw std::runtime_error("Failed to write to socket.\n");
+      throw std::runtime_error("Failed to write to socket.");
 
    struct pollfd fds{};
    fds.events = POLLIN;
    fds.fd = fd;
 
    if (poll(&fds, 1, 1000) < 0)
-      throw std::runtime_error("Failed to poll socket.\n");
+      throw std::runtime_error("Failed to poll socket.");
 
    if (!(fds.revents & POLLIN))
-      throw std::runtime_error("Poll timed out.\n");
+      throw std::runtime_error("Poll timed out.");
 
    char buf[1024];
    ssize_t ret = read(fd, buf, sizeof(buf));
 
    if (ret <= 0)
-      throw std::runtime_error("Failed to read reply.\n");
+      throw std::runtime_error("Failed to read reply.");
 
    std::string str{buf, buf + ret};
    str.erase(str.rfind("\r\n"));
